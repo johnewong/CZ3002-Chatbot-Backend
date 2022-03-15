f8d449bc026e33ec1497b9463f64021c5b421f60
yfweng <johnewong95@gmail.com>
2022-03-15 22:59:37 +0800

    add unit test dependency

--- bot/src/test/java/chatbot/BotUnitTest.java
+++ bot/src/test/java/chatbot/BotUnitTest.java
@@ -1,6 +1,25 @@
-package ;
+package chatbot;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
 
 public class BotUnitTest {
 
+    String currentDir = System.getProperty("user.dir");
+    Bot bot;
 
+
+    @BeforeEach
+    void setUp() {
+        bot =  new Bot("0", currentDir + "\\chatbotData\\data.xml", currentDir + "\\chatbotData\\faq.xml", currentDir + "\\chatbotData\\rating.xml");
+    }
+
+    @Test
+    @DisplayName("Simple test")
+    void testBot() {
+        assertEquals(0,0);
+    }
 }
